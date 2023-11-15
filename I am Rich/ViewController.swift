7ae4af58d45b8263841e356c95fd79a3eb6e11fd
Muhammad Wasiq <muhammadwasiq@ab-MacBook-Pro.local>
2023-11-15 10:45:12 +0500

    Section-2 First App

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Muhammad Wasiq  on 01/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
